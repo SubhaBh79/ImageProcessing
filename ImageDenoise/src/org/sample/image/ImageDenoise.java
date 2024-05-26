e0a73cdc64c77905600a6fe557d85a09a95bf14f
Subhadeep <sbhattacharjee7991@gmail.com>
2024-05-26 15:44:28 +0530

    different operation

--- ImageDenoise/src/org/sample/image/ImageDenoise.java
+++ ImageDenoise/src/org/sample/image/ImageDenoise.java
@@ -18,7 +18,26 @@ public class ImageDenoise {
 		try {
 			BufferedImage srcImg = imgReadWrite.readRGBImage(args[0]);
 			List<Map<String, Integer>> rgbArray= imgCon.imageToRGBArray(srcImg);
-			imgReadWrite.writeRGBImage(args[1], imgCon.rgbArrayToImage(processor.changeImagePixel(rgbArray,10000)));
+			BufferedImage desImage = null;
+			switch (args[2]) {
+			case "CHANGE_PIXEL":
+				int pixelValue = Integer.valueOf(args[3]);
+				desImage = imgCon.rgbArrayToImage(processor.changeImagePixel(rgbArray,pixelValue));
+				break;
+			case "CHANGE_HUE":
+				desImage = imgCon.rgbArrayToImage(processor.setHue(rgbArray,args[3]));
+				break;
+			case "NEGATIVE":
+				desImage = imgCon.rgbArrayToImage(processor.createNegativeRGB(rgbArray));
+				break;
+			case "GRAYSCALE":
+				desImage = imgCon.rgbArrayToImage(processor.createGrayscaleValue(rgbArray));
+				break;
+			default:
+				break;
+			}
+			imgReadWrite.writeRGBImage(args[1],desImage);
+			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
--- ImageDenoise/src/org/sample/image/process/ImageProcessor.java
+++ ImageDenoise/src/org/sample/image/process/ImageProcessor.java
@@ -16,4 +16,40 @@ public class ImageProcessor {
 		}
 		return rgbArray;
 	}
+	
+	public List<Map<String, Integer>> setHue(List<Map<String, Integer>> rgbArray, String hueValue) {
+		for (Map<String, Integer> rgbEntry : rgbArray) {
+			for (Map.Entry<String, Integer> entry : rgbEntry.entrySet()) {
+				if(entry.getKey() == Constant.ALPHA) {
+					continue;
+				}
+				else if(entry.getKey() == hueValue) {
+					continue;
+				}
+				else {
+					rgbEntry.put(entry.getKey(), 0);
+				}
+			}
+		}
+		return rgbArray;
+	}
+	
+	public List<Map<String, Integer>> createNegativeRGB(List<Map<String, Integer>> rgbArray) {
+		for (Map<String, Integer> rgbEntry : rgbArray) {
+			rgbEntry.put(Constant.RED, 255 - rgbEntry.get(Constant.RED));
+			rgbEntry.put(Constant.GREEN, 255 - rgbEntry.get(Constant.GREEN));
+			rgbEntry.put(Constant.BLUE, 255 - rgbEntry.get(Constant.BLUE));
+		}
+		return rgbArray;
+	}
+	
+	public List<Map<String, Integer>> createGrayscaleValue(List<Map<String, Integer>> rgbArray) {
+		for (Map<String, Integer> rgbEntry : rgbArray) {
+			int avg = (rgbEntry.get(Constant.RED) + rgbEntry.get(Constant.GREEN) + rgbEntry.get(Constant.BLUE)) /3;
+			rgbEntry.put(Constant.RED, avg);
+			rgbEntry.put(Constant.GREEN, avg);
+			rgbEntry.put(Constant.BLUE, avg);
+		}
+		return rgbArray;
+	}
 }
